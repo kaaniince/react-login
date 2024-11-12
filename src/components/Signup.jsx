142ca4dda2f8dbb938cf131f2d072f1e07d0648e
Kaan Ince <kaaniince@outlook.com>
2024-11-13 01:29:55 +0300

    style edited

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useFormik } from "formik";
-import validationSchema from "./validations";
+import validationSchema from "./Validations";
+import "../index.css";
 
 function Signup() {
   const { handleSubmit, values, handleChange, errors, touched, handleBlur } =
@@ -11,52 +12,56 @@ function Signup() {
         passwordConfirm: "",
       },
       onSubmit: (value) => {
-        console.log(values);
+        console.log(value);
       },
       validationSchema,
     });
+
   return (
-    <div>
+    <div className="container">
       <h1>Sign Up</h1>
       <form onSubmit={handleSubmit}>
         <label>Email</label>
         <input
+          className="input-field"
           name="email"
           value={values.email}
           onChange={handleChange}
           onBlur={handleBlur}
         />
-        {errors.email && touched.email && <div>{errors.email}</div>}
-        <br />
-        <br />
+        {errors.email && touched.email && (
+          <div className="error-message">{errors.email}</div>
+        )}
+
         <label>Password</label>
         <input
+          className="input-field"
           name="password"
+          type="password"
           value={values.password}
           onChange={handleChange}
           onBlur={handleBlur}
         />
-        {errors.password && touched.password && <div>{errors.password}</div>}
-        <br />
-        <br />
+        {errors.password && touched.password && (
+          <div className="error-message">{errors.password}</div>
+        )}
+
         <label>Confirm Password</label>
         <input
+          className="input-field"
           name="passwordConfirm"
+          type="password"
           value={values.passwordConfirm}
           onChange={handleChange}
           onBlur={handleBlur}
         />
         {errors.passwordConfirm && touched.passwordConfirm && (
-          <div>{errors.passwordConfirm}</div>
+          <div className="error-message">{errors.passwordConfirm}</div>
         )}
-        <br />
+
         <button type="submit">Submit</button>
-        <br />
-        <br />
-        {JSON.stringify(values)}
       </form>
     </div>
   );
 }
-
 export default Signup;
